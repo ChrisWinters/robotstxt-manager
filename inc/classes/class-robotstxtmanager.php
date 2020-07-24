75fc10cf9d8bde06d03eb2815552f8f3933ea448
ChrisWinters <chrisw@null.net>
2020-07-24 08:43:41 -0400

    Corrects Robotstxt class instance callable

--- inc/classes/class-robotstxtmanager.php
+++ inc/classes/class-robotstxtmanager.php
@@ -11,31 +11,31 @@
 
 namespace RobotstxtManager;
 
+use RobotstxtManager\Robotstxt;
+
 if ( false === defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
- * Plugin Core
+ * Robots.txt Manager
  */
 final class RobotstxtManager {
 	/**
 	 * Init Plugin
 	 */
 	public static function init() {
-		// Display Robots.txt File.
 		$robotstxt = new Robotstxt();
 
-		/*
+		/**
 		 * Determines whether the current request is for an administrative interface page.
-		 * https://developer.wordpress.org/reference/functions/is_admin/
+		 *
+		 * @source https://developer.wordpress.org/reference/functions/is_admin/
 		 */
 		if ( true === is_admin() ) {
-			// Save Plugin Admin Data.
 			$admin_save = new Plugin_Admin_Save();
 			$admin_save->init();
 
-			// Display Plugin Admin.
 			$admin = new Plugin_Admin();
 			$admin->init();
 		}
