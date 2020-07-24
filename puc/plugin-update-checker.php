52a686f08d7721d1d6d94edba7c8fe15113b59d7
ChrisWinters <chrisw@null.net>
2020-07-24 08:35:21 -0400

    Update PUC from v4 to v4.9

--- puc/plugin-update-checker.php
+++ puc/plugin-update-checker.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * Plugin Update Checker Library 4.4
+ * Plugin Update Checker Library 4.9
  * http://w-shadow.com/
  *
  * Copyright 2017 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  *
- * @package Plugin Update Checker
+ * @package Puc
  */
 
-// v4p4/Factory.
-require dirname( __FILE__ ) . '/Puc/v4p4/Factory.php';
+require dirname( __FILE__ ) . '/v4p9/Autoloader.php';
+new Puc_v4p9_Autoloader();
 
-// v4/Factory.
-require dirname( __FILE__ ) . '/Puc/v4/Factory.php';
+require dirname( __FILE__ ) . '/v4p9/Factory.php';
+require dirname( __FILE__ ) . '/v4/Factory.php';
 
-// Autoloader.
-require dirname( __FILE__ ) . '/Puc/v4p4/Autoloader.php';
-
-new Puc_v4p4_Autoloader();
-
-// Register classes defined in this file with the factory.
-Puc_v4_Factory::addVersion( 'Plugin_UpdateChecker', 'Puc_v4p4_Plugin_UpdateChecker', '4.4' );
-Puc_v4_Factory::addVersion( 'Theme_UpdateChecker', 'Puc_v4p4_Theme_UpdateChecker', '4.4' );
-
-Puc_v4_Factory::addVersion( 'Vcs_PluginUpdateChecker', 'Puc_v4p4_Vcs_PluginUpdateChecker', '4.4' );
-Puc_v4_Factory::addVersion( 'Vcs_ThemeUpdateChecker', 'Puc_v4p4_Vcs_ThemeUpdateChecker', '4.4' );
-
-Puc_v4_Factory::addVersion( 'GitHubApi', 'Puc_v4p4_Vcs_GitHubApi', '4.4' );
-Puc_v4_Factory::addVersion( 'BitBucketApi', 'Puc_v4p4_Vcs_BitBucketApi', '4.4' );
-Puc_v4_Factory::addVersion( 'GitLabApi', 'Puc_v4p4_Vcs_GitLabApi', '4.4' );
+foreach (
+	array(
+		'Plugin_UpdateChecker'    => 'Puc_v4p9_Plugin_UpdateChecker',
+		'Theme_UpdateChecker'     => 'Puc_v4p9_Theme_UpdateChecker',
+		'Vcs_PluginUpdateChecker' => 'Puc_v4p9_Vcs_PluginUpdateChecker',
+		'Vcs_ThemeUpdateChecker'  => 'Puc_v4p9_Vcs_ThemeUpdateChecker',
+		'GitHubApi'               => 'Puc_v4p9_Vcs_GitHubApi',
+		'BitBucketApi'            => 'Puc_v4p9_Vcs_BitBucketApi',
+		'GitLabApi'               => 'Puc_v4p9_Vcs_GitLabApi',
+	)
+	as $puc_general_class => $puc_versioned_class
+) {
+	Puc_v4_Factory::addVersion( $puc_general_class, $puc_versioned_class, '4.9' );
+	Puc_v4p9_Factory::addVersion( $puc_general_class, $puc_versioned_class, '4.9' );
+}
