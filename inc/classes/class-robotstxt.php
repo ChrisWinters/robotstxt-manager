2357343b8bd28839360a11d0ff75921ee9d43467
ChrisWinters <chrisw@null.net>
2020-07-25 15:43:06 -0400

    Refactor class and robots.txt detection

--- inc/classes/class-robotstxt.php
+++ inc/classes/class-robotstxt.php
@@ -20,38 +20,62 @@ if ( false === defined( 'ABSPATH' ) ) {
  */
 final class Robotstxt {
 	/**
-	 * Maybe display robots.txt
+	 * Maybe start robots.txt display
 	 */
 	public function __construct() {
-		if ( true === $this->is_robots_txt_file() ) {
-			$this->robotstxt();
+		if ( true === $this->is_robotstxt_file() ) {
+			$this->display();
 		}
 	}
 
 
 	/**
-	 * Echo robots.txt file
+	 * Display robots.txt file
 	 */
-	private function robotstxt() {
-		/**
-		 * If Multisite is enabled.
-		 *
-		 * @source https://developer.wordpress.org/reference/functions/is_multisite/
-		 */
-		if ( true === is_multisite() ) {
-			/**
-			 * Retrieve the current site ID.
-			 *
-			 * @source https://developer.wordpress.org/reference/functions/get_current_blog_id/
-			 */
-			$site_id = get_current_blog_id();
+	private function display() {
+		$robotstxt   = $this->get_robotstxt();
+		$blog_public = \get_option( 'blog_public' );
 
+		if ( '0' === $blog_public ) {
+			$robotstxt = "Disallow: /\n";
+		}
+
+		if ( true !== empty( $robotstxt ) ) {
+			header( 'Status: 200 OK', true, 200 );
+			header( 'Content-Type: text/plain; charset=utf-8' );
+
+			/**
+			 * Escaping for HTML blocks.
+			 *
+			 * @source https://developer.wordpress.org/reference/functions/esc_html/
+			 */
+			echo \esc_html( $robotstxt );
+			exit;
+		}
+	}
+
+
+	/**
+	 * Get robots.txt file
+	 *
+	 * @return string
+	 */
+	private function get_robotstxt() : string {
+		/**
+		 * Retrieve the current site ID.
+		 *
+		 * @source https://developer.wordpress.org/reference/functions/get_current_blog_id/
+		 */
+		$blog_id = \get_current_blog_id();
+
+		if ( $blog_id > 0 &&
+			true === function_exists( 'switch_to_blog' ) ) {
 			/**
 			 * Switch the current blog.
 			 *
 			 * @source https://developer.wordpress.org/reference/functions/switch_to_blog/
 			 */
-			switch_to_blog( $site_id );
+			\switch_to_blog( $blog_id );
 		}
 
 		/**
@@ -59,59 +83,48 @@ final class Robotstxt {
 		 *
 		 * @source https://developer.wordpress.org/reference/functions/get_option/
 		 */
-		$website_option = get_option( ROBOTSTXT_MANAGER_PLUGIN_NAME );
-		$robotstxt_file = '';
+		$settings = \get_option( ROBOTSTXT_MANAGER_PLUGIN_NAME );
 
-		if ( true !== empty( $website_option['disable'] ) ) {
-			return;
-		}
-
-		if ( true !== empty( $website_option['robotstxt'] ) ) {
-			$robotstxt_file = $website_option['robotstxt'];
-		}
-
-		if ( true !== empty( $robotstxt_file ) ) {
-			header( 'Status: 200 OK', true, 200 );
-			header( 'Content-Type: text/plain; charset=utf-8' );
-
+		if ( $blog_id > 0 &&
+			true === function_exists( 'restore_current_blog' ) ) {
 			/**
-			 * Fires when displaying the robots.txt file.
+			 * Restore the current blog, after calling restore_current_blog().
 			 *
-			 * @since 2.1.0
+			 * @source https://developer.wordpress.org/reference/functions/restore_current_blog/
 			 */
-			do_action( 'do_robotstxt' );
+			\restore_current_blog();
+		}
 
-			$public = get_option( 'blog_public' );
+		$robotstxt = '';
+
+		if ( true !== empty( $settings['disable'] ) ) {
+			return null;
+		}
+
+		if ( true !== empty( $settings['robotstxt'] ) ) {
+			$robotstxt = $settings['robotstxt'];
+		}
+
+		if ( true !== empty( $robotstxt ) ) {
+			$public = \get_option( 'blog_public' );
 
 			if ( '0' === $public ) {
-				$output .= "Disallow: /\n";
-			} else {
-				$output = $robotstxt_file;
+				$robotstxt = "Disallow: /\n";
 			}
-
-			/**
-			 * Filters the robots.txt output.
-			 *
-			 * @since 1.1.0
-			 *
-			 * @param string $output Robots.txt output.
-			 * @param bool   $public Whether the site is considered "public".
-			 */
-			echo apply_filters(
-				'robotstxt_manager_robots_txt',
-				$output,
-				$public
-			);
-
-			exit;
 		}
+
+		if ( true !== empty( $user_robotstxt ) ) {
+			$robotstxt = $user_robotstxt;
+		}
+
+		return (string) $robotstxt;
 	}
 
 
 	/**
 	 * Check if called file is robots.txt file
 	 */
-	private function is_robots_txt_file() : bool {
+	private function is_robotstxt_file() : bool {
 		if ( true === filter_has_var( INPUT_SERVER, 'REQUEST_URI' ) ) {
 			$filename = filter_input(
 				INPUT_SERVER,
@@ -121,7 +134,12 @@ final class Robotstxt {
 			);
 		} else {
 			if ( isset( $_SERVER['REQUEST_URI'] ) ) {
-				$request_uri = $_SERVER['REQUEST_URI'];
+				/**
+				 * Remove slashes from a string or array of strings.
+				 *
+				 * @source https://developer.wordpress.org/reference/functions/wp_unslash/
+				 */
+				$request_uri = \wp_unslash( $_SERVER['REQUEST_URI'] );
 				$filename    = filter_var(
 					$request_uri,
 					FILTER_UNSAFE_RAW,
