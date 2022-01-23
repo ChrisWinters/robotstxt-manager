482880e6581713f3649e76b36dabe3edb0b1ffae
Chris Winters <webmaster.skills@gmail.com>
2022-01-23 10:29:01 -0500

    Updated plugin update checker

--- puc/plugin-update-checker.php
+++ puc/plugin-update-checker.php
@@ -1,32 +1,10 @@
 <?php
 /**
- * Plugin Update Checker Library 4.9
+ * Plugin Update Checker Library 4.11
  * http://w-shadow.com/
  *
- * Copyright 2017 Janis Elsts
+ * Copyright 2021 Janis Elsts
  * Released under the MIT license. See license.txt for details.
- *
- * @package Puc
  */
 
-require dirname( __FILE__ ) . '/v4p9/Autoloader.php';
-new Puc_v4p9_Autoloader();
-
-require dirname( __FILE__ ) . '/v4p9/Factory.php';
-require dirname( __FILE__ ) . '/v4/Factory.php';
-
-foreach (
-	array(
-		'Plugin_UpdateChecker'    => 'Puc_v4p9_Plugin_UpdateChecker',
-		'Theme_UpdateChecker'     => 'Puc_v4p9_Theme_UpdateChecker',
-		'Vcs_PluginUpdateChecker' => 'Puc_v4p9_Vcs_PluginUpdateChecker',
-		'Vcs_ThemeUpdateChecker'  => 'Puc_v4p9_Vcs_ThemeUpdateChecker',
-		'GitHubApi'               => 'Puc_v4p9_Vcs_GitHubApi',
-		'BitBucketApi'            => 'Puc_v4p9_Vcs_BitBucketApi',
-		'GitLabApi'               => 'Puc_v4p9_Vcs_GitLabApi',
-	)
-	as $puc_general_class => $puc_versioned_class
-) {
-	Puc_v4_Factory::addVersion( $puc_general_class, $puc_versioned_class, '4.9' );
-	Puc_v4p9_Factory::addVersion( $puc_general_class, $puc_versioned_class, '4.9' );
-}
+require dirname(__FILE__) . '/load-v4p11.php';
