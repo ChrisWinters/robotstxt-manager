18d1c2cc29551be590bd28400efb191aea7992b9
Chris Winters <webmaster.skills@gmail.com>
2022-01-23 15:16:34 -0500

    Setup inquirer to ask version number

--- _src/version.js
+++ _src/version.js
@@ -1,71 +1,70 @@
 #!/usr/bin/env node
 
 /*!
- * Copy wordpress files from project to theme.
+ * Update plugin version.
  */
 
 'use strict'
 
+const inquirer = require('inquirer')
+
 // Configuration.
 const packageJson = require('../package.json')
-const currentVersion = packageJson.currentVersion
-const previousVersion = packageJson.previousVersion
+const packageVersion = packageJson.version
 
-// Tags to update.
-const currentStableTag = 'Stable tag: ' + currentVersion
-const previousStableTag = 'Stable tag: ' + previousVersion
-
-const currentChangeLogTag = '### ' + currentVersion
-const previousChangeLogTag = '### ' + previousVersion
-
-const currentHeaderTag = 'Version: ' + currentVersion
-const previousHeaderTag = 'Version: ' + previousVersion
-
-const currentConstantTag = '\'' + currentVersion + '\''
-const previousConstantTag = '\'' + previousVersion + '\''
-
-const currentUpdatesTag = '"' + currentVersion + '"'
-const previousUpdatesTag = '"' + previousVersion + '"'
-
-const currentPackageTag = '"' + currentVersion + '"'
-const previousPackageTag = '"' + previousVersion + '"'
-
-try {
-  const replace = require('replace-in-file')
-
-  // All child theme files.
-  const files = [
-    './readme.txt',
-    './README.md',
-    './robotstxt-manager.php',
-    './updates.json',
-    './package.json',
-  ]
-
-  // Update version.
-  const updateVersions = {
-    files: files,
-    from: [
-      previousStableTag,
-      previousChangeLogTag,
-      previousHeaderTag,
-      previousConstantTag,
-      previousUpdatesTag,
-      previousPackageTag
-    ],
-    to: [
-      currentStableTag,
-      currentChangeLogTag,
-      currentHeaderTag,
-      currentConstantTag,
-      currentUpdatesTag,
-      currentPackageTag
-    ],
-  };
-
-  replace.sync(updateVersions)
-
-  console.log('Version updated to: ' + currentVersion )
-} catch(error) {
-    console.log('Packages missing: Type `npm install`\n')
-}
+inquirer.prompt([{
+  type: 'input',
+  name: 'version',
+  message: 'What version are we moving to? (Current version is ' + packageVersion + ')'
+}]).then(function(res) {
+  try {
+    const replace = require('replace-in-file')
+  
+    // Tags to update.
+    const currentStableTag = 'Stable tag:** ' + res.version
+    const previousStableTag = 'Stable tag:** ' + packageVersion
+  
+    const currentChangeLogTag = '### ' + res.version
+    const previousChangeLogTag = '### ' + packageVersion
+  
+    const currentHeaderTag = 'Version: ' + res.version
+    const previousHeaderTag = 'Version: ' + packageVersion
+  
+    const currentConstantTag = '\'' + res.version + '\''
+    const previousConstantTag = '\'' + packageVersion + '\''
+  
+    const currentUpdatesTag = '"' + res.version + '"'
+    const previousUpdatesTag = '"' + packageVersion + '"'
+  
+    // Update version.
+    replace.sync(
+      {
+        files: [
+          './readme.txt',
+          './README.md',
+          './robotstxt-manager.php',
+          './updates.json',
+          './package.json',
+        ],
+        from: [
+          previousStableTag,
+          previousChangeLogTag,
+          previousHeaderTag,
+          previousConstantTag,
+          previousUpdatesTag
+        ],
+        to: [
+          currentStableTag,
+          currentChangeLogTag,
+          currentHeaderTag,
+          currentConstantTag,
+          currentUpdatesTag
+        ],
+      }
+    )
+  
+    console.log('Version updated to: ' + res.version )
+  } catch(error) {
+      console.log('Packages missing: Type `npm install`\n')
+  }
+})
