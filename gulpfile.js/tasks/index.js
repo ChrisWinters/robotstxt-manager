e9ed7c87ea1cb4d8a2ed09387b144a74d465fc6d
Chris Winters <webmaster.skills@gmail.com>
2022-01-23 15:24:21 -0500

    Removed to use npm scripts

--- gulpfile.js/tasks/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Make an index.js in a directory with this code to clean things up:
- * @source https://github.com/aseemk/requireDir
- */
-module.exports = require('require-dir')(); // defaults to '.'
