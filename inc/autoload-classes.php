2593d03da2e1f8376cecdf08fb0ea13845c0b1e2
Chris Winters <webmaster.skills@gmail.com>
2022-11-23 21:04:09 -0500

    Removed old autoloader

--- inc/autoload-classes.php
+++ inc/autoload-classes.php
@@ -40,33 +40,4 @@ function RegisterClasses($class)
     }
 }
 
-/**
- * Register Classes.
- *
- * @param string $class Loaded Classes.
- */
-function robotstxtmanager_register_classes($class)
-{
-    // Namespace Prefix.
-    $prefix = 'RobotstxtManager\\';
-
-    // Move To Next Rgistered autoloader.
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    // Build Class Name.
-    $relativeClass = strtolower(str_replace('_', '-', substr($class, $len)));
-
-    // Replace Dir Separators and Replace Namespace with Base Dir.
-    $file = ROBOTSTXT_MANAGER_DIR.'/inc/classes/class-'.str_replace('\\', '/', $relativeClass).'.php';
-
-    // Include File.
-    if (true === file_exists($file)) {
-        require $file;
-    }
-}
-
-spl_autoload_register('RobotstxtManager\robotstxtmanager_register_classes');
 spl_autoload_register('RobotstxtManager\RegisterClasses');
