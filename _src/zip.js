c0ace11ed6514799501832664a6615296cec6810
Chris Winters <webmaster.skills@gmail.com>
2022-11-26 16:34:21 -0500

    Adjusted to import package format

--- _src/zip.js
+++ _src/zip.js
@@ -4,74 +4,74 @@
  * Zip project for release
  */
 
-'use strict'
+import fs from "node:fs";
+import archiver from "archiver";
 
-const fs = require('fs-extra')
+const createZipFile = async () => {
+  try {
+    // create a file to stream archive data to.
+    const output = fs.createWriteStream("./robotstxt-manager.zip");
+    const archive = archiver("zip", {
+      zlib: { level: 9 }, // Sets the compression level.
+    });
 
-try {
-  // require modules
-  const archiver = require('archiver')
+    // listen for all archive data to be written
+    // 'close' event is fired only when a file descriptor is involved
+    output.on("close", function () {
+      console.log(archive.pointer() + " total bytes");
+      console.log(
+        "archiver has been finalized and the output file descriptor has closed."
+      );
+    });
 
-  // create a file to stream archive data to.
-  const output = fs.createWriteStream('./robotstxt-manager.zip');
-  const archive = archiver('zip', {
-    zlib: { level: 9 } // Sets the compression level.
-  });
+    // This event is fired when the data source is drained no matter what was the data source.
+    // It is not part of this library but rather from the NodeJS Stream API.
+    // @see: https://nodejs.org/api/stream.html#stream_event_end
+    output.on("end", function () {
+      console.log("Data has been drained");
+    });
 
-  // listen for all archive data to be written
-  // 'close' event is fired only when a file descriptor is involved
-  output.on('close', function() {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-  });
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on("warning", function (err) {
+      if (err.code === "ENOENT") {
+        // log warning
+      } else {
+        // throw error
+        throw err;
+      }
+    });
 
-  // This event is fired when the data source is drained no matter what was the data source.
-  // It is not part of this library but rather from the NodeJS Stream API.
-  // @see: https://nodejs.org/api/stream.html#stream_event_end
-  output.on('end', function() {
-    console.log('Data has been drained');
-  });
-
-  // good practice to catch warnings (ie stat failures and other non-blocking errors)
-  archive.on('warning', function(err) {
-    if (err.code === 'ENOENT') {
-      // log warning
-    } else {
-      // throw error
+    // good practice to catch this error explicitly
+    archive.on("error", function (err) {
       throw err;
-    }
-  });
+    });
 
-  // good practice to catch this error explicitly
-  archive.on('error', function(err) {
-    throw err;
-  });
-
-  // append files from a sub-directory, putting its contents at the root of archive
-  archive.glob('**/*', 
-    {
-      cwd: '../robotstxt-manager',
+    // append files from a sub-directory, putting its contents at the root of archive
+    archive.glob("**/*", {
+      cwd: "../robotstxt-manager",
       ignore: [
-        'svn/**',
-        '_src/**',
-        'gulpfile.js/**',
-        'node_modules/**',
-        'LICENSE',
-        '*.md',
-        '*.json',
-        '*.zip',
-        '',
-        ''
-      ]
-    }
-  );
+        "svn/**",
+        "_src/**",
+        "gulpfile.js/**",
+        "node_modules/**",
+        "LICENSE",
+        "*.md",
+        "*.json",
+        "*.zip",
+        "",
+        "",
+      ],
+    });
 
-  // pipe archive data to the file
-  archive.pipe(output);
+    // pipe archive data to the file
+    archive.pipe(output);
 
-  // finalize the archive (ie we are done appending files but streams have to finish yet)
-  // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-  archive.finalize();
-} catch(error) {
-    console.log('Packages missing: Type `npm install`\n')
-}
+    // finalize the archive (ie we are done appending files but streams have to finish yet)
+    // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+    archive.finalize();
+  } catch (error) {
+    console.log("Packages missing: Type `npm install`\n");
+  }
+};
+
+createZipFile();
