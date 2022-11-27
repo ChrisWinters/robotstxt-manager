e51747c83233e2fb5afcb7d165137af82e8228ab
Chris Winters <webmaster.skills@gmail.com>
2022-11-26 21:41:42 -0500

    New function to return current date and time

--- _src/version.js
+++ _src/version.js
@@ -12,6 +12,31 @@ import fs from 'node:fs/promises';
 const packageJson = JSON.parse(await fs.readFile('package.json'));
 const packageVersion = packageJson.version;
 
+const getDateTime = function() {
+  const dateObject = new Date();
+
+  // current date
+  // adjust 0 before single digit date
+  const date = ("0" + dateObject.getDate()).slice(-2);
+
+  // current month
+  const month = ("0" + (dateObject.getMonth() + 1)).slice(-2);
+
+  // current year
+  const year = dateObject.getFullYear();
+
+  // current hours
+  const hours = dateObject.getHours();
+
+  // current minutes
+  const minutes = dateObject.getMinutes();
+
+  // current seconds
+  const seconds = dateObject.getSeconds();
+
+  return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+}
+
 inquirer
   .prompt([
     {
