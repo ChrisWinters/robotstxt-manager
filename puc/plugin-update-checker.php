cfa8dbc3fe82b164bcd1d41f19b50196dd224bd1
Chris Winters <webmaster.skills@gmail.com>
2022-11-27 21:11:28 -0500

    Updated PUC to 5.0

--- puc/plugin-update-checker.php
+++ puc/plugin-update-checker.php
@@ -1,10 +1,9 @@
 <?php
 /**
  * Plugin Update Checker Library 5.0
- * http://w-shadow.com/
+ * http://w-shadow.com/.
  *
  * Copyright 2022 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  */
-
-require dirname(__FILE__) . '/load-v5p0.php';
+require dirname(__FILE__).'/load-v5p0.php';
