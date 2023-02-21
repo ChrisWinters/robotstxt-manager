57d0165d8603d1fe209943e6f672faba01306884
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 18:12:53 -0500

    Added robots.txt help links

--- inc/templates/sidebar.php
+++ inc/templates/sidebar.php
@@ -31,6 +31,13 @@ if (false === defined('ABSPATH')) {
 	</div>
 	<div class="inside" style="clear:both;padding-top:1px;">
 		<div class="para">
+			<ul>
+				<li>&bull; <a href="https://wordpress.org/support/article/search-engine-optimization/#robots-txt-optimization" target="_blank"><?php \esc_html_e('Robots.txt Optimization Tips', 'robotstxt-manager'); ?></a></li>
+				<li>&bull; <a href="http://www.askapache.com/seo/wordpress-robotstxt-seo/" target="_blank"><?php \esc_html_e('AskAapche Robots.txt Example', 'robotstxt-manager'); ?></a></li>
+				<li>&bull; <a href="https://developers.google.com/webmasters/control-crawl-index/docs/faq" target="_blank"><?php \esc_html_e('Google Robots.txt F.A.Q.', 'robotstxt-manager'); ?></a></li>
+				<li>&bull; <a href="https://developers.google.com/webmasters/control-crawl-index/docs/robots_txt" target="_blank"><?php \esc_html_e('Robots.txt Specifications', 'robotstxt-manager'); ?></a></li>
+				<li>&bull; <a href="http://www.robotstxt.org/db.html" target="_blank"><?php \esc_html_e('Web Robots Database', 'robotstxt-manager'); ?></a></li>
+			</ul>
 		</div>
 	</div>
 </div>
