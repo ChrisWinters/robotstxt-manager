f9510a76b3b3e3ced41c364eb9134794f8af443d
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 18:12:14 -0500

    Updated plugin details

--- inc/templates/sidebar.php
+++ inc/templates/sidebar.php
@@ -17,6 +17,10 @@ if (false === defined('ABSPATH')) {
 	</div>
 	<div class="inside" style="clear:both;padding-top:1px;">
 		<div class="para">
+			<ul>
+				<li>&bull; <a href="https://github.com/ChrisWinters/robotstxt-manager" target="_blank"><?php \esc_html_e('Plugin Home Page', 'robotstxt-manager'); ?></a></li>
+				<li>&bull; <a href="https://github.com/ChrisWinters/robotstxt-manager/issues" target="_blank"><?php \esc_html_e('Bugs & Feature Request', 'robotstxt-manager'); ?></a></li>
+			</ul>
 		</div>
 	</div>
 </div>
