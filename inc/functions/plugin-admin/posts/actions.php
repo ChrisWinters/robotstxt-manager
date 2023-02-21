298004a37b0f11e1a6a34a07ff413293dc55174d
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 18:24:45 -0500

    New posts actions function

---
+++ inc/functions/plugin-admin/posts/actions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Private admin area function.
+ */
+
+namespace RobotstxtManager\PluginAdmin\Posts;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Maybe update plugin settings, then return user back to plugin admin area.
+ */
+function actions(): void
+{
+    return;
+}
