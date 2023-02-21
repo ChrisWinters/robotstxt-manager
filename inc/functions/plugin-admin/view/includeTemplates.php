db268af11735142d88e714dc501232f6d6e349b3
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 17:33:47 -0500

    New function to include plugin templates

---
+++ inc/functions/plugin-admin/view/includeTemplates.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Public admin area function: view.
+ */
+
+namespace RobotstxtManager\PluginAdmin\View;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Include plugin admin area templates.
+ */
+function includeTemplates(): void
+{
+    return;
+}
