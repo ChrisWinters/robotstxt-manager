0f6b31aaf1386ca81701e34114a4ea5eb71341f5
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 17:04:01 -0500

    New register plugin function

---
+++ inc/functions/registerPlugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Backend function.
+ */
+
+namespace RobotstxtManager;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Register plugin features..
+ */
+function registerPlugin(): void
+{
+    return;
+}
