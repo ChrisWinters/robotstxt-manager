2af52a1571cfa05e2efc8778bb0954f2a92d0586
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 17:39:14 -0500

    Added include for templates

--- robotstxt-manager.php
+++ robotstxt-manager.php
@@ -46,6 +46,7 @@ require_once __DIR__.'/inc/functions/option/update.php';
 require_once __DIR__.'/inc/functions/plugin-admin/queryString.php';
 
 require_once __DIR__.'/inc/functions/plugin-admin/view/displayAdmin.php';
+require_once __DIR__.'/inc/functions/plugin-admin/view/includeTemplates.php';
 
 // Init plugin.
 \add_action(
