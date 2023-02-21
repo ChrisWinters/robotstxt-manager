0e195ccb5f261c267ad69595a0e3718d1975846f
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 18:07:30 -0500

    Improved return to top  accessibility

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -22,9 +22,13 @@ if (false === defined('ABSPATH')) {
 			<br class="clear" />
 		</div><!-- #post-body -->
 	</div><!-- #poststuff -->
-	<div class="clearfix">
-		<div class="float-left text-left"><small>&#9829; <?php echo \esc_html(\RobotstxtManager\settings('plugin_name')); ?> - <?php \esc_html_e('Version', 'robotstxt-manager'); ?> <?php echo \esc_html(\RobotstxtManager\settings('plugin_version')); ?></small></div>
-		<div class="float-right text-right"><a href="#top"><span class="dashicons-before dashicons-arrow-up"><?php \esc_html_e('top', 'robotstxt-manager'); ?></span></a></div>
+    <div class="clearfix">
+		<div class="float-start text-left">
+			<small>&#9829; <?php echo \esc_html(\RobotstxtManager\settings('plugin_name')); ?> - <?php \esc_html_e('Version', 'robotstxt-manager'); ?> <?php echo \esc_html(\RobotstxtManager\settings('plugin_version')); ?></small>
+		</div>
+		<div class="float-end text-right">
+			<a href="#wpbody-content" aria-label="<?php \esc_html_e('Click to return to plugin settings.', 'robotstxt-manager'); ?>"><span class="dashicons-before dashicons-arrow-up"><?php \esc_html_e('top', 'robotstxt-manager'); ?></span></a>
+		</div>
 	</div>
 </div><!-- .wrap -->
 
