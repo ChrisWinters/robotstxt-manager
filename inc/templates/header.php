9b3f35cac02dc0408a8e5d795a22da68652eaa34
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 20:37:21 -0500

    Added navigation tabs display

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -15,6 +15,8 @@ if (false === defined('ABSPATH')) {
     <h2><span class="dashicons dashicons-admin-site-alt3 mt-1 pt-1"></span> <?php
         echo \esc_html(\RobotstxtManager\settings('plugin_name')); ?> &#8594; <small><?php
         echo \esc_html(\RobotstxtManager\settings('plugin_about')); ?></small></h2>
+ 
+    <?php echo \RobotstxtManager\PluginAdmin\View\navigationTabs(); ?>
 
     <div id="poststuff">
         <div id="post-body" class="metabox-holder columns-2">
