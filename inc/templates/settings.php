266a173934cbb179bc51aee9ab478e33027ad8d0
Chris Winters <webmaster.skills@gmail.com>
2023-02-21 19:15:59 -0500

    Added form fields for presets

--- inc/templates/settings.php
+++ inc/templates/settings.php
@@ -42,6 +42,62 @@ if (false === defined('ABSPATH')) {
 <?php \submit_button(\esc_html__('update robots.txt file', 'robotstxt-manager')); ?>
 </form>
 
+<hr />
+
+<form enctype="multipart/form-data" method="post" action="<?php echo admin_url('admin-post.php'); ?>">
+<?php
+\wp_nonce_field(
+    'robotstxt_manager_action',
+    'robotstxt_manager_nonce'
+);
+
+?>
+<input type="hidden" name="action" value="preset" />
+<input type="hidden" name="tab" value="settings" />
+
+<table class="form-table">
+	<tbody>
+		<tr>
+		<td colspan="2">
+			<div class="text-dark font-weight-bold p-0 m-0 h5"><?php \esc_html_e('Robots.txt file presets', 'robotstxt-manager'); ?></div>
+			<p class="description"><?php \esc_html_e('Select a WordPress ready preset robots.txt file to load and use.', 'robotstxt-manager'); ?></p>
+		</td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="simplified"><?php \esc_html_e('Simplified robots.txt file', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="simplified" id="simplified" /> <span class="description"><?php \esc_html_e('Simplified rules that will work with most WordPress setups.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="alternative"><?php \esc_html_e('Alternative robots.txt file', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="alternative" id="alternative" /> <span class="description"><?php \esc_html_e('Similar to the simplified robots.txt file, with more disallows.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="wordpress"><?php \esc_html_e('WordPress robots.txt file', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="wordpress" id="wordpress" /> <span class="description"><?php \esc_html_e('Includes all possible rules, use as a base to create a custom file.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="blogger"><?php \esc_html_e('Blogger friendly robots.txt file', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="blogger" id="blogger" /> <span class="description"><?php \esc_html_e('Optimized for blog focused WordPress websites.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="google"><?php \esc_html_e('Google friendly robots.txt file', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="google" id="google" /> <span class="description"><?php \esc_html_e('A Google friendly robots.txt file.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="open"><?php \esc_html_e('Open robots.txt file', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="open" id="open" /> <span class="description"><?php \esc_html_e('Fully open robots.txt file, no disallows.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+		<tr>
+		<th scope="row"><label for="blocked"><?php \esc_html_e('Disallow entire website', 'robotstxt-manager'); ?></label></th>
+		<td><input type="radio" name="preset" value="blocked" id="blocked" /> <span class="description"><?php \esc_html_e('Disallow everything, prevent spiders from indexing the website.', 'robotstxt-manager'); ?></span></td>
+		</tr>
+	</tbody>
+</table>
+
+<?php \submit_button(\esc_html__('change robots.txt file', 'robotstxt-manager')); ?>
+
+</form>
+
 <br /><br /><hr />
 
 <form enctype="multipart/form-data" method="post" action="<?php echo admin_url('admin-post.php'); ?>">
